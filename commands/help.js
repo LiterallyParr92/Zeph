a5eaff770f47c5615917fdfa735a282903779dbe
gelatini <gelatini92@gmail.com>
2026-02-07 14:24:34 -0500

    help fix

---
+++ commands/help.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('help')
+    .setDescription('💡 | Muestra la lista de comandos disponibles'),
+
+  async execute(interaction) {
+
+    const embed = new EmbedBuilder()
+      .setTitle('📖 | Comandos del bot')
+      .setColor('Blue')
+      .setDescription('Aquí tienes la lista de comandos disponibles:')
+      .addFields(
+        { name: '/ping', value: 'Muestra la latencia del bot' },
+      )
+      .setFooter({ text: 'Zeph • desarrollada por ♱ - Parra' });
+
+    await interaction.reply({ embeds: [embed], ephemeral: true });
+  }
+};
